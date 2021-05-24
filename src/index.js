8884bc64064ca1493c70f51bc5be6a9c59ae68ab
ZHANG LEI <JP25718@w022571904977m.local>
2021-05-25 00:08:30 +0900

    Update to Material Kit React latest

--- src/index.js
+++ src/index.js
@@ -2,44 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch } from "react-router-dom";
-import jwtDecode from "jwt-decode";
-import { Provider } from "react-redux";
-import store from "./store";
-import { login } from "./store/actions";
-import Cookies from "./utils/Cookies";
 
-import "./assets/scss/material-kit-react.scss?v=1.4.0";
+import "assets/scss/material-kit-react.scss?v=1.10.0";
 
 // pages for this product
-import Components from "views/Components/Components.jsx";
-import LandingPage from "views/LandingPage/LandingPage.jsx";
-import ProfilePage from "views/ProfilePage/ProfilePage.jsx";
-import LoginPage from "views/LoginPage/LoginPage.jsx";
-// svea
-import SveaUi from "views/SveaUI/SveaUI.jsx";
+import Components from "views/Components/Components.js";
+import LandingPage from "views/LandingPage/LandingPage.js";
+import ProfilePage from "views/ProfilePage/ProfilePage.js";
+import LoginPage from "views/LoginPage/LoginPage.js";
 
 var hist = createBrowserHistory();
 
-const token = Cookies.read("token");
-
-if (token) {
-  const user = jwtDecode(token);
-
-  store.dispatch(login(user));
-}
-
 ReactDOM.render(
-  <Provider store={store}>
-    <Router history={hist}>
-      <Switch>
-        <Route path="/landing-page" component={LandingPage} />
-        <Route path="/profile-page" component={ProfilePage} />
-        <Route path="/login-page" component={LoginPage} />
-        <Route path="/components" component={Components} />
-        <Route path="/sveaui" component={SveaUi} />
-        <Route path="/" component={SveaUi} />
-      </Switch>
-    </Router>
-  </Provider>,
+  <Router history={hist}>
+    <Switch>
+      <Route path="/landing-page" component={LandingPage} />
+      <Route path="/profile-page" component={ProfilePage} />
+      <Route path="/login-page" component={LoginPage} />
+      <Route path="/" component={Components} />
+    </Switch>
+  </Router>,
   document.getElementById("root")
 );
